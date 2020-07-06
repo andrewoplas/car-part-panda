6733266241900f82b72e8637e88bb1f5fbcbb1b1
Andrew Oplas <andrewmoplas@gmail.com>
2020-07-06 23:21:28 +0800

    Add create car

--- resources/views/pages/create-car.blade.php
+++ resources/views/pages/create-car.blade.php
@@ -1,38 +1,36 @@
-@extends('layouts.create', ['backPageLink' => 'create', '$backPageName' => 'Terung'])
+@extends('layouts.create', ['backPageLink' => '/create', 'backPageName' => 'Terung'])
 
 @section('create-content')
-    <div class="Create">
-        <h3 class="page-title">Mijn biedingen</h3>
+    <div class="CreateCar">
+        <h3 class="create-title">Welke auto wil je <span class="alt">verkopen?</span></h3>
 
-        <div class="list">
-            @for($i=0; $i<6; $i++)
-                <x-product-card-vertical
-                        image="wheel-thumbnail.png"
-                        alt="thumbnail"
-                        title="Nissan Skyline R34"
-                        price="25.000"
-                        year="2016"
-                        bid="Hoogste bod nu € 23.540"
-                        bidding
-                ></x-product-card-vertical>
-            @endfor
+        <div class="normal-spacing">
+            <x-label class="space-bottom" title="MERK"></x-label>
+            <x-select
+                    class="block"
+                    placeholder="Maak je keuze"
+                    :options="['value1' => 'Option 1','value2' => 'Option 2','value3' => 'Option 3']"
+            ></x-select>
         </div>
 
-        <div class="list mobile">
-            @for($i=0; $i<4; $i++)
-                <x-product-card-horizontal
-                        image="wheel-thumbnail.png"
-                        alt="thumbnail"
-                        title="Nissan Skyline R34"
-                        price="25.000"
-                        kilometers="30.000"
-                        year="2016"
-                        bid="Hoogste bod nu € 23.540"
-                        bidding
-                ></x-product-card-horizontal>
-            @endfor
+        <div class="normal-spacing">
+            <x-label class="space-bottom" title="MODEL"></x-label>
+            <x-select
+                    class="block"
+                    placeholder="Maak je keuze"
+                    :options="['value1' => 'Option 1','value2' => 'Option 2','value3' => 'Option 3']"
+            ></x-select>
         </div>
 
-        <x-pagination class="pagination"></x-pagination>
+        <div class="large-spacing">
+            <x-label class="space-bottom" title="EDITIE"></x-label>
+            <x-select
+                    class="block"
+                    placeholder="Maak je keuze"
+                    :options="['value1' => 'Option 1','value2' => 'Option 2','value3' => 'Option 3']"
+            ></x-select>
+        </div>
+
+        <x-button class="btn-submit" title="Bewaren"></x-button>
     </div>
 @endsection
